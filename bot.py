30393ce372c45925faecd7d5bd930d10f0d6f95b
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-05-14 13:42:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,11 +12,12 @@ m = Client("m", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 @m.on_message(filters.command("d", prefixes="/"))
 async def send(Client, m: Message):
     try:
-        current = ""
-        total = ""
-        k = await Client.download_media(message=m.reply_to_message, progress=await m.edit(f"{current * 100 / total:.1f}%"))
-        await Client.send_document(m.chat.id, k, progress=await m.edit(f"{current * 100 / total:.1f}%"))
+        k = await Client.download_media(message=m.reply_to_message, progress=prog)
+        await Client.send_document(m.chat.id, k, progress=prog)
     except Exception as e:
         await m.reply(e)
+
+def prog(current, total, m: Message):
+    message.edit(m=f"{current * 100 / total:.1f}%")
 
 m.run()
